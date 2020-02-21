cfaa36f62b5233545333a28a7ef7a9be087bca92
sergeyarhipov <s.m.arhipov@mail.ru>
2020-02-21 22:43:23 +0500

    Добавлен фокус к полю ввода "Ваше имя" при открытии popup

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
 var popupLink = document.querySelector(".btn-map");
 var popupSend = document.querySelector(".popup-send");
 var popupClose= document.querySelector(".popup-close");
+var userName = popupSend.querySelector("[name=user-name]") 
 
 popupLink.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	popupSend.classList.add("popup-show");
+	userName.focus();
 });
 
 popupClose.addEventListener("click", function(evt) {
