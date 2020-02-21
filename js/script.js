34e46f987ed5839cb210fc7e2650d0af8ff5e382
sergeyarhipov <s.m.arhipov@mail.ru>
2020-02-21 22:30:22 +0500

    Добавлен обработчик события клика мыши по кнопке закрытия popup

--- js/script.js
+++ js/script.js
@@ -5,4 +5,9 @@ var popupClose= document.querySelector(".popup-close");
 popupLink.addEventListener("click", function(evt) {
 	evt.preventDefault();
 	popupSend.classList.add("popup-show");
-})
+});
+
+popupClose.addEventListener("click", function(evt) {
+	evt.preventDefault();
+	popupSend.classList.remove("popup-show");
+});
