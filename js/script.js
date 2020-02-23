60b624a7d5c8b9a1a299df6cce967945b0f1aa4f
sergeyarhipov <s.m.arhipov@mail.ru>
2020-02-23 21:08:28 +0500

    Добавлена карта

--- js/script.js
+++ js/script.js
@@ -4,6 +4,13 @@ var popupClose= document.querySelector(".popup-close");
 var letterPopup = popupSend.querySelector("form");
 var userName = letterPopup.querySelector("[name=user-name]");
 var userEmail = letterPopup.querySelector("[name=user-email]");
+var frameMap = document.querySelector("iframe");
+var labelMap = document.querySelector(".map-label")
+
+frameMap.addEventListener("mouseover", function(evt) {
+	evt.preventDefault();
+	labelMap.classList.add("map-label-hidden"); 
+});
 
 popupLink.addEventListener("click", function(evt) {
 	evt.preventDefault();
